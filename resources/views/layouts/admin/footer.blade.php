9dd9cc5a81bd2918741deaad68bbe4c573d93fc8
shivamk12051997 <shivamk12051997@gmail.com>
2025-05-15 13:19:28 +0530

    Template Setup

--- resources/views/layouts/admin/footer.blade.php
+++ resources/views/layouts/admin/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-md-6 footer-copyright text-center">
-          <p class="mb-0">All Rights Reserved © 2022 {{ env('APP_NAME') }}  </p>
+          <p class="mb-0">All Rights Reserved © {{ date('Y') }} {{ env('APP_NAME') }}  </p>
         </div>
         <div class="col-md-6 footer-copyright text-center">
           <p class="mb-0">Made with ❤️ by <a href="https://digitaldarzee.com/" target="_blank" rel="noopener noreferrer">DigitalDarzee</a> </p>
